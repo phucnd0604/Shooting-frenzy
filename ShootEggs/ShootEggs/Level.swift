209cfc26b9c029d852a6d53b92fc18adb442e69a
freezer10 <phuc@HYHY.local>
2015-04-16 23:00:45 +0700

    comment

--- ShootEggs/ShootEggs/Level.swift
+++ ShootEggs/ShootEggs/Level.swift
@@ -26,15 +26,15 @@ class Level {
   private func createInitialCookies() -> Set<EggNote> {
     var set = Set<EggNote>()
     
-    // 1
+    // 1 get col and row
     for row in 0..<rowsNumber {
       for column in 0..<columsNumber {        
-        // 2
+        // 2 create random egg type
         var eggType = EggType.random()
-        // 3
+        // 3 create egg and add to eggs array
         let egg = EggNote(imageName: eggType.eggName, column: column, row: row, type: eggType)
         eggs[column, row] = egg
-        // 4
+        // 4 
         set.insert(egg)
       }
     }
